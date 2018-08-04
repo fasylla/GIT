674c0689ecdd5e4886c3d7e972401dc758f71c15
tima <syllafatoumata29@yahoo.fr>
2018-08-04 00:08:19 -0400

    ajout de l'age

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
 	public static void main(String[] args){
-		System.out.println("Hello everybody");
+		int age=2;
+		System.out.println("Hello everybody j'ai "+age+" ans");
 	}
 }
